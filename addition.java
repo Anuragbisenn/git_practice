0f6150d0c8e6a64024fb2500fe4df7d639577eb1
Anuragbisenn <anuragbisenofficial@gmail.com>
2022-05-16 22:35:20 +0530

    adding java

---
+++ addition.java
@@ -0,0 +1,17 @@
+ class addition {
+
+    public  int addi(int a,int b)
+    { 
+        int c =a+b;
+        return c;
+    }
+    
+}
+ 
+ class subtr
+{
+    public int sub (int a, int b)
+    {
+        return a-b;
+    }
+}
